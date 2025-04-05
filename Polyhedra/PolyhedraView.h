faca3b7a2037796dfdf85ed1efcbfb9d71d10d3b
Gregory Casamento <greg.casamento@gmail.com>
2025-04-04 20:19:15 -0400

    Initial Commit

---
+++ Polyhedra/PolyhedraView.h
@@ -0,0 +1,12 @@
+//
+//  PolyhedraView.h
+//  Polyhedra
+//
+//  Created by Gregory John Casamento on 4/4/25.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface PolyhedraView : ScreenSaverView
+
+@end
---
+++ Polyhedra/PolyhedraView.m
@@ -0,0 +1,51 @@
+//
+//  PolyhedraView.m
+//  Polyhedra
+//
+//  Created by Gregory John Casamento on 4/4/25.
+//
+
+#import "PolyhedraView.h"
+
+@implementation PolyhedraView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
